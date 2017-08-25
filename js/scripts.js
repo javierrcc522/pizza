2fec4b2f2dd0c30fbf575b5a13cdbfaf184c7c4f
Javier Jimenez <javierrcc522@gmail.com>
2017-08-25 13:18:29 -0700

    add a fully functional prototype to the js file to add all the prices

--- js/scripts.js
+++ js/scripts.js
@@ -5,28 +5,38 @@ function Orderpizza(pizza, type, toppin) {
   this.toppin = toppin;
 };
 
-Orderpizza.prototype.pizza = function () {
-  if (this.pizza === "largePizza") {
+Orderpizza.prototype.moneyPizza = function () {
+  if (this.pizza === "Large") {
     return 15;
-  } else if (this.pizza === "midPizza") {
+  } else if (this.pizza === "Medium") {
     return 10;
   } else {
     return 7;
   };
 };
 
-Orderpizza.prototype.type = function () {
-  if (this.type === "organicPizza"){
+Orderpizza.prototype.moneyType = function () {
+  if (this.type === "organic"){
     return 5;
   } else {
     return 0;
   };
 };
 
-Orderpizza.prototype.Toppin = function () {
+Orderpizza.prototype.moneyToppin = function () {
   if (this.toppin === "cheese") {
-    return 1
+    return 3
+  }
+  else if (this.toppin === "ham") {
+    return 4
+  } else {
+    return 5
   };
+};
+
+Orderpizza.prototype.finalPrice = function () {
+  return 0 + this.moneyPizza() + this.moneyType() + this.moneyToppin()
+
 };
 
 
@@ -43,5 +53,6 @@ $(document).ready(function() {
     $("#showPizza").text(newPizza.pizza);
     $("#showIngre").text(newPizza.type);
     $("#showToppin").text(newPizza.toppin);
+    $("#showPrice").text(newPizza.finalPrice());
   });
 });
